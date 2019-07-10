3ec91d5367feed46d264201bcc5bff6a0612a0a6
Stanley Luong <slluong@uw.edu>
2019-07-10 21:20:12 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -5,25 +5,25 @@ def map(array)
   end
 end
 
-# def reduce(array, memo = nil)
-#   if memo
-#     i = 0 
-#   else
-#     memo = array[0]
-#     i = 1
-#   end
-#   while i < array.length do 
-#     memo = yield memo, array[i]
-#     i += 1
-#   end
-#   return memo
-# end
-
-def reduce(array, s=nil)
-  if !s
-    return array.reduce(s) do |memo,n|
-      yield memo, n
+def reduce(array, memo = nil)
+  if memo
+    i = 0 
+  else
+    memo = array[0]
+    i = 1
   end
-else s = array.shift
-    
+  while i < array.length do 
+    memo = yield memo, array[i]
+    i += 1
+  end
+  return memo
 end
+
+# def reduce(array, s=nil)
+#   if !s
+#     return array.reduce(s) do |memo,n|
+#       yield memo, n
+#   end
+# else s = array.shift
+    
+# end
