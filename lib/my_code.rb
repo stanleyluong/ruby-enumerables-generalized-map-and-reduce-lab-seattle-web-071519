c7d5a0a3aad838fe065b84bae17249cd60bd4b57
Stanley Luong <slluong@uw.edu>
2019-07-10 21:04:09 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,21 @@
 # Your Code Here
+def map(array)
+  return array.map do |n|
+     yield n
+  end
+end
+
+def reduce(array, memo = 0)
+  i = 0 
+  while i < array.length do 
+    memo = yield memo, array[i]
+    i += 1
+  end
+  return memo
+end
+
+# def add(x,y)
+#   x + y
+# end
+
+# 1+= 1
